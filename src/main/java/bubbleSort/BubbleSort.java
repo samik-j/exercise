fd92725b14a8d480304813b35fc9c00386135dc1
joanna <jo.senczuk@gmail.com>
2017-11-21 15:04:44 +0000

    Bubble sort

---
+++ src/main/java/bubbleSort/BubbleSort.java
@@ -0,0 +1,20 @@
+package bubbleSort;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BubbleSort {
+
+    static void sort(List<Integer> list) {
+        int n = list.size();
+
+        do {
+            for (int i = 0; i < n - 1; i++) {
+                if (list.get(i) > list.get(i + 1)) {
+                    Collections.swap(list, i, i + 1);
+                }
+            }
+            n = n - 1;
+        } while (n > 1);
+    }
+}
---
+++ src/test/java/bubbleSort/BubbleSortTest.java
@@ -0,0 +1,37 @@
+package bubbleSort;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BubbleSortTest {
+
+    @Test
+    public void shouldSortWhenOrderIsReversed() {
+        // given
+        List<Integer> list = Arrays.asList(5, 4, 3, 2, 1);
+
+        // when
+        BubbleSort.sort(list);
+
+        // then
+        List<Integer> result = Arrays.asList(1, 2, 3, 4, 5);
+        assertEquals(result, list);
+    }
+
+    @Test
+    public void shouldSortWhenOrderIsRandom() {
+        // given
+        List<Integer> list = Arrays.asList(3, 2, 4, 1, 5);
+
+        // when
+        BubbleSort.sort(list);
+
+        // then
+        List<Integer> result = Arrays.asList(1, 2, 3, 4, 5);
+        assertEquals(result, list);
+    }
+}
